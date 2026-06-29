578f52302235f5aec30e8c632ba3020ffa60d92b
Bruno Wendel Marcolino <bruno.marcolino@instramed.com.br>
2026-06-29 19:55:27 -0300

    refactor(layout): header e footer compartilhados

---
+++ assets/js/layout.js
@@ -0,0 +1,109 @@
+function renderHeader() {
+
+document.write(`
+
+<header class="site-header">
+
+<a href="index.html" class="brand">
+
+Daiane <span>Rodrigues</span>
+
+</a>
+
+<nav class="nav">
+
+<a href="index.html">Início</a>
+
+<a href="sobre.html">Sobre</a>
+
+<a href="blog.html">Blog</a>
+
+<a href="categorias.html">Categorias</a>
+
+<a href="agendamento.html">Agendamento</a>
+
+<a href="contato.html">Contato</a>
+
+</nav>
+
+</header>
+
+`);
+
+}
+
+function renderFooter(){
+
+document.write(`
+
+<footer class="footer">
+
+<div class="footer-container">
+
+<div>
+
+<h3>Daiane Rodrigues</h3>
+
+<p>
+
+Psicanálise, escuta e acolhimento.
+
+</p>
+
+</div>
+
+<div>
+
+<h4>Categorias</h4>
+
+<ul>
+
+<li>Ansiedade</li>
+
+<li>Casais</li>
+
+<li>Maternidade</li>
+
+<li>Autoconhecimento</li>
+
+</ul>
+
+</div>
+
+<div>
+
+<h4>Contato</h4>
+
+<p>
+
+WhatsApp
+
+</p>
+
+<p>
+
+Instagram
+
+</p>
+
+<p>
+
+Email
+
+</p>
+
+</div>
+
+</div>
+
+<p class="copyright">
+
+© 2026 Daiane Rodrigues
+
+</p>
+
+</footer>
+
+`);
+
+}
