f77c961816c49eed2da583039823d56a4ec445f8
Bruno Wendel Marcolino <bruno.marcolino@instramed.com.br>
2026-06-29 19:47:13 -0300

    feat(blog): cria listagem profissional

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,30 @@
-﻿const search = document.querySelector('#search');
-const posts = [...document.querySelectorAll('#posts .card')];
+﻿const campo = document.querySelector("#search");
 
-if (search) {
-  search.addEventListener('input', () => {
-    const term = search.value.toLowerCase();
-    posts.forEach(post => {
-      post.style.display = post.dataset.title.includes(term) ? 'block' : 'none';
-    });
-  });
+if (campo) {
+
+const cards =
+document.querySelectorAll(".post-card");
+
+campo.addEventListener("keyup", function () {
+
+const texto =
+this.value.toLowerCase();
+
+cards.forEach(card => {
+
+const base =
+card.dataset.title;
+
+if(base.includes(texto))
+
+card.style.display="block";
+
+else
+
+card.style.display="none";
+
+});
+
+});
+
 }
